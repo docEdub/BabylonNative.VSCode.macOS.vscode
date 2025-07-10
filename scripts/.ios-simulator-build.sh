586f8e5361fd5bd899d8d7d100bd5574090fc55d
Andy Fillebrown <andy@docEdub.com>
2025-07-09 22:40:08 -0400

    Try using compile_commands.json for c_cpp_properties.json include paths

--- scripts/.ios-simulator-build.sh
+++ scripts/.ios-simulator-build.sh
@@ -5,4 +5,11 @@ if [ -z "$1" ]; then
 fi
 TARGET="$1"
 
+# Quiet build.
+# xcodebuild -project .build/iOS/BabylonNative.xcodeproj -scheme $TARGET -sdk iphonesimulator -destination 'arch=arm64,OS=18.5,name=iPhone 16 Pro Max' build -quiet
+
+# Use xcpretty.
 xcodebuild -project .build/iOS/BabylonNative.xcodeproj -scheme $TARGET -sdk iphonesimulator -destination 'arch=arm64,OS=18.5,name=iPhone 16 Pro Max' build -quiet
+
+# Use xcpretty and generate a compile_commands.json file.
+# xcodebuild -project .build/iOS/BabylonNative.xcodeproj -scheme $TARGET -sdk iphonesimulator -destination 'arch=arm64,OS=18.5,name=iPhone 16 Pro Max' build | xcpretty -r json-compilation-database --output .vscode/.build/compile_commands.json
